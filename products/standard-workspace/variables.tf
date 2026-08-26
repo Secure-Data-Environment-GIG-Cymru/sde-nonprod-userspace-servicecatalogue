aaabcaca3c44eff5bd58ab8a0485c224e2473a01
Jen-Selby <78080377+Jen-Selby@users.noreply.github.com>
2026-08-26 15:27:55 +0100

    update - add project id validation

--- products/standard-workspace/variables.tf
+++ products/standard-workspace/variables.tf
@@ -1,6 +1,11 @@
 variable "project_id" {
   description = "The GCP project ID in which the Workbench will be created. Must be in the format ndr-sde-nonprod-<project-name>, for example ndr-sde-nonprod-userproject4."
   type        = string
+
+    validation {
+    condition     = can(regex("^ndr-sde-nonprod-[a-z0-9-]+$", var.project_id))
+    error_message = "project_id must be in the format ndr-sde-nonprod-<project-name>, for example ndr-sde-nonprod-userproject4."
+    }
 }
 
 #variable "project_name" {
