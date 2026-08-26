ab31948b611bfdf79a92a617b441439e029d405b
Jen-Selby <78080377+Jen-Selby@users.noreply.github.com>
2026-08-26 15:22:22 +0100

    update - change inputs

--- products/standard-workspace/main.tf
+++ products/standard-workspace/main.tf
@@ -11,12 +11,17 @@ locals {
   region = "europe-west2"
   zone   = "europe-west2-a"
 
-  network_name = "sde-nonprod-${var.project_name}-network"
-  subnet_name  = "sde-nonprod-${var.project_name}-subnet"
+  project_short_name = trimprefix(var.project_id, "ndr-sde-nonprod-")
+
+  network_name = "sde-nonprod-${local.project_short_name}-network"
+  subnet_name  = "sde-nonprod-${local.project_short_name}-subnet"
+
+  #network_name = "sde-nonprod-${var.project_name}-network"
+  #subnet_name  = "sde-nonprod-${var.project_name}-subnet"
 
   service_account_email = "sde-nonprod-scdeployer-adn-sa@ndr-sde-nonprod-seedp.iam.gserviceaccount.com"
 
-  workbench_name = "${var.project_name}-standard-workbench-${var.user_id}"
+  workbench_name = "${local.project_short_name}-standard-workbench-${var.user_id}"
 
   machine_type = "n2-standard-4"
 
